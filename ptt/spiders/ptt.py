4dcf12c0017b3019fb22104f0b9c95e22f7452a4
yoyo890121 <yoyo890121@hotmail.com>
2017-06-07 13:02:51 +0800

    Initial commit

---
+++ ptt/spiders/ptt.py
@@ -0,0 +1,129 @@
+import logging
+
+from datetime import datetime
+import scrapy
+import re
+from six import u
+from bs4 import BeautifulSoup
+from scrapy.http import FormRequest
+
+from ptt.items import PostItem
+from scrapy.conf import settings
+
+class PTTSpider(scrapy.Spider):
+    name = 'ptt'
+    allowed_domains = ['ptt.cc']
+    start_urls = ('https://www.ptt.cc/bbs/' + settings.get('BOARD') + '/index.html', )
+
+    _retries = 0
+    MAX_RETRY = 1
+
+    _pages = 0
+
+    def parse(self, response):
+        if len(response.xpath('//div[@class="over18-notice"]')) > 0:
+            if self._retries < PTTSpider.MAX_RETRY:
+                self._retries += 1
+                logging.warning('retry {} times...'.format(self._retries))
+                yield FormRequest.from_response(response,
+                                                formdata={'yes': 'yes'},
+                                                callback=self.parse)
+            else:
+                logging.warning('you cannot pass')
+
+        else:
+            self._pages += 1
+            for href in response.css('.r-ent > div.title > a::attr(href)'):
+                url = response.urljoin(href.extract())
+                yield scrapy.Request(url, callback=self.parse_post)
+
+            if self._pages < settings.get('MAX_PAGES'):
+                next_page = response.xpath(
+                    '//div[@id="action-bar-container"]//a[contains(text(), "上頁")]/@href')
+                if next_page:
+                    url = response.urljoin(next_page[0].extract())
+                    logging.warning('follow {}'.format(url))
+                    yield scrapy.Request(url, self.parse)
+                else:
+                    logging.warning('no next page')
+            else:
+                logging.warning('max pages reached')
+
+    def parse_post(self, response):
+        item = PostItem()
+        item['title'] = response.xpath(
+            '//meta[@property="og:title"]/@content')[0].extract()
+        item['author'] = response.xpath(
+            '//div[@class="article-metaline"]/span[text()="作者"]/following-sibling::span[1]/text()')[
+                0].extract().split(' ')[0]
+        datetime_str = response.xpath(
+            '//div[@class="article-metaline"]/span[text()="時間"]/following-sibling::span[1]/text()')[
+                0].extract()
+        item['date'] = datetime.strptime(datetime_str, '%a %b %d %H:%M:%S %Y')
+
+        # main_content = response.xpath('//div[@id="main-content"]//text()')[0].extract()
+
+        soup = BeautifulSoup(response.text, 'html.parser')
+        main_content = soup.select("#main-content")[0]
+        metas = main_content.select("div.article-metaline")
+
+        if metas:
+            # remove meta nodes
+            for meta in metas:
+                meta.extract()
+            for meta in main_content.select('div.article-metaline-right'):
+                meta.extract()
+
+        # remove and keep push nodes
+        pushes = main_content.find_all('div', class_='push')
+        for push in pushes:
+            push.extract()
+
+        try:
+            ip = main_content.find(text=re.compile(u'※ 發信站:'))
+            ip = re.search('[0-9]*\.[0-9]*\.[0-9]*\.[0-9]*', ip).group()
+        except:
+            ip = "None"
+
+        # 移除 '※ 發信站:' (starts with u'\u203b'), '◆ From:' (starts with u'\u25c6'), 空行及多餘空白
+        # 保留英數字, 中文及中文標點, 網址, 部分特殊符號
+        filtered = [v for v in main_content.stripped_strings if v[0] not in [u'※', u'◆'] and v[:2] not in [u'--']]
+        expr = re.compile(u(
+            r'[^\u4e00-\u9fa5\u3002\uff1b\uff0c\uff1a\u201c\u201d\uff08\uff09\u3001\uff1f\u300a\u300b\s\w:/-_.?~%()]'))
+        for i in range(len(filtered)):
+            filtered[i] = re.sub(expr, '', filtered[i])
+
+        filtered = [_f for _f in filtered if _f]  # remove empty strings
+        filtered = [x for x in filtered if response.url not in x]  # remove last line containing the url of the article
+        content = ' '.join(filtered)
+        content = re.sub(r'(\s)+', ' ', content)
+        # print 'content', content
+
+        item['content'] = content
+
+        comments = []
+        total_score = 0
+        for comment in response.xpath('//div[@class="push"]'):
+            push_tag = comment.css('span.push-tag::text')[0].extract()
+            push_user = comment.css('span.push-userid::text')[0].extract()
+            push_content = comment.css('span.push-content::text')[0].extract()
+
+            if '推' in push_tag:
+                score = 1
+            elif '噓' in push_tag:
+                score = -1
+            else:
+                score = 0
+
+            total_score += score
+
+            comments.append({'user': push_user,
+                             'content': push_content,
+                             'score': score})
+
+        item['comments'] = comments
+        item['score'] = total_score
+        item['url'] = response.url
+        item['ip'] = ip
+
+        yield item
